a09fd22f61999f5fe078359a281ef76eca066c33
Jetmon Deng <jetmondeng@Jetmons-MacBook-Pro.local>
2023-01-10 01:18:27 -0500

    update

---
+++ static/resources/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("lunarnetYoutube").addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=JuP8PNZsAcM")
+    })
+    document.getElementById("atiReplit").addEventListener("click", () => {
+        window.open("https://replit.com/@oktres/ATIinterpreter-Public-Version?v=1")
+    })
+})
