20186fc0980dd1db94bfed6b8bc0916164ad12b7
Jetmon Deng <jetmondeng@Jetmons-MacBook-Pro.local>
2023-02-14 10:39:29 -0500

    project test

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -12,6 +12,7 @@ const attachPortfolio = (app) => {
             template = template.replace("[{Project Icon}]", database[req.query.page].icon)
             template = template.replace("[{Project QuickSummary}]", database[req.query.page].quickSummary)
             template = template.replace("[{Project Links}]", WebsiteButton(database[req.query.page].links.website) + GitHubButton(database[req.query.page].links.github))
+            template = template.replace("[{ProjectAbout}]", database[req.query.page].description)
             res.send(template)
         } else {  
             res.sendFile(__dirname + "/home.html")
